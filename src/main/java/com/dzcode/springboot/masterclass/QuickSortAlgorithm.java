32bb7083d58fddaa2612646b13522ef0fcf57768
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-11 09:50:06 +0100

    @Primary

--- src/main/java/com/dzcode/springboot/masterclass/QuickSortAlgorithm.java
+++ src/main/java/com/dzcode/springboot/masterclass/QuickSortAlgorithm.java
@@ -2,7 +2,7 @@ package com.dzcode.springboot.masterclass;
 
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class QuickSortAlgorithm implements SortAlgorithm {
     public int[] sort(int[] numbers) {
         return numbers;

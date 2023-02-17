cd015e09a24ce9b11b98a540c9f4f659a7aa5179
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-17 12:08:04 +0100

    defining spring application context using xml

--- src/main/java/com/dzcode/springboot/masterclass/SpringbootMasterclassXmlApplication.java
+++ src/main/java/com/dzcode/springboot/masterclass/SpringbootMasterclassXmlApplication.java
@@ -4,13 +4,8 @@ import com.dzcode.springboot.masterclass.scope.PersonDAO;
 import com.dzcode.springboot.masterclass.xml.XmlPersonDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class SpringbootMasterclassXmlApplication {
 
 	public static Logger LOGGER = LoggerFactory.getLogger(SpringbootMasterclassXmlApplication.class);

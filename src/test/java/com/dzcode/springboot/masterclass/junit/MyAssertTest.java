111e2dc637e5046bdddc3a3eb86957fef4be2c2a
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-17 12:53:24 +0100

    other assert methods

---
+++ src/test/java/com/dzcode/springboot/masterclass/junit/MyAssertTest.java
@@ -0,0 +1,24 @@
+package com.dzcode.springboot.masterclass.junit;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyAssertTest {
+    List<String> todos = Arrays.asList("AWS", "Azure", "DevOps");
+    @Test
+    void assertTest() {
+        boolean test = todos.contains("AWS");
+        boolean test2 = todos.contains("GCP");
+
+        assertTrue(test);
+        assertFalse(test2);
+
+        assertArrayEquals(new int[] {2, 1}, new int[] {2, 1});
+
+        assertEquals(3, todos.size());
+    }
+}

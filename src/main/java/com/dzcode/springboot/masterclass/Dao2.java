a12e766d4964961d14014f9bbd7a08ec50e3ef7a
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-18 00:17:22 +0100

    setting up aop project for cross cutting concept

---
+++ src/main/java/com/dzcode/springboot/masterclass/Dao2.java
@@ -0,0 +1,10 @@
+package com.dzcode.springboot.masterclass;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class Dao2 {
+    public String retrieveSomething() {
+        return "Dao2";
+    }
+}

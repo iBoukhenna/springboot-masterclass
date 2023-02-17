b62414a15e4479d9f8ec54faafe7a4121530bc0f
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-17 16:06:34 +0100

    simple rest api

---
+++ src/main/java/com/dzcode/springboot/masterclass/CourseController.java
@@ -0,0 +1,20 @@
+package com.dzcode.springboot.masterclass;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "Learn AWS"),
+                new Course(2, "Learn DevOps", "Learn DevOps")
+        );
+    }
+
+}

1945361affb0ae66280cf2907b73eba3713ec926
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-17 12:07:35 +0100

    defining spring application context using xml

---
+++ src/main/java/com/dzcode/springboot/masterclass/xml/XmlJdbcConnection.java
@@ -0,0 +1,7 @@
+package com.dzcode.springboot.masterclass.xml;
+
+public class XmlJdbcConnection {
+    public XmlJdbcConnection() {
+        System.out.println("Xml JDBC Connection");
+    }
+}
---
+++ src/main/java/com/dzcode/springboot/masterclass/xml/XmlPersonDAO.java
@@ -0,0 +1,14 @@
+package com.dzcode.springboot.masterclass.xml;
+
+public class XmlPersonDAO {
+
+    XmlJdbcConnection xmlJdbcConnection;
+
+    public XmlJdbcConnection getXmlJdbcConnection() {
+        return xmlJdbcConnection;
+    }
+
+    public void setXmlJdbcConnection(XmlJdbcConnection xmlJdbcConnection) {
+        this.xmlJdbcConnection = xmlJdbcConnection;
+    }
+}

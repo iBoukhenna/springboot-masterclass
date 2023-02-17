ff31ebb6fe48044fb83a16ec6782bb4f344572c1
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-17 14:43:13 +0100

    add new senarios

--- src/test/java/com/dzcode/springboot/masterclass/business/SomeBusinessImplTest.java
+++ src/test/java/com/dzcode/springboot/masterclass/business/SomeBusinessImplTest.java
@@ -13,6 +13,14 @@ public class SomeBusinessImplTest {
         int result = someBusinessImpl.findTheGreatestFromAllData();
         assertEquals(25, result);
     }
+
+    @Test
+    void findTheGreatestFromAllDataTest_withOneValue() {
+        DataServiceStub_withOneValue dataServiceStub = new DataServiceStub_withOneValue();
+        SomeBusinessImpl someBusinessImpl = new SomeBusinessImpl(dataServiceStub);
+        int result = someBusinessImpl.findTheGreatestFromAllData();
+        assertEquals(25, result);
+    }
 }
 
 class DataServiceStub implements DataService {
@@ -20,5 +28,12 @@ class DataServiceStub implements DataService {
     @Override
     public int[] retrieveAllData() {
         return new int[] {25, 15, 5};
+    }
+}
+class DataServiceStub_withOneValue implements DataService {
+
+    @Override
+    public int[] retrieveAllData() {
+        return new int[] {5};
     }
 }

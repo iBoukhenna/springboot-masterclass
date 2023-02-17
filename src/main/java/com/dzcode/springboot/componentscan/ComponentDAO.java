199e563b04ee5cfafebeb91ae4090fa288492739
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-17 12:28:58 +0100

    @Component, @Repository, @Service, @Controller to add specifics thing to specifics layer IOC Container  : inversion of control it creates the objects, configures and manages life cycle uses the Dependecy injection Bean Factory are implementation of IOC Container Application Context is Bean Factory+++ (Spring AOP / I18n capabilities / WebAplicationContext)

--- src/main/java/com/dzcode/springboot/componentscan/ComponentDAO.java
+++ src/main/java/com/dzcode/springboot/componentscan/ComponentDAO.java
@@ -1,9 +1,9 @@
 package com.dzcode.springboot.componentscan;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class ComponentDAO {
 
     @Autowired

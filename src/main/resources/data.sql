23b65893ff279e78d2ce88ea04fd961574dd26cc
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-18 09:11:38 +0100

    add data to table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,11 @@ create table person
     birth_date timestamp,
     primary key(id)
 );
+
+
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10001,  'Ranga', 'Hyderabad',CURRENT_TIMESTAMP());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10002,  'James', 'New York',CURRENT_TIMESTAMP());
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE )
+VALUES(10003,  'Pieter', 'Amsterdam',CURRENT_TIMESTAMP());

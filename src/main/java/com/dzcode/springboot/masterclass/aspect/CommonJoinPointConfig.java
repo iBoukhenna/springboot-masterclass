07491cbb6c89515a67dfca2e776336b0ed741247
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-18 08:41:32 +0100

    other Pointcuts

--- src/main/java/com/dzcode/springboot/masterclass/aspect/CommonJoinPointConfig.java
+++ src/main/java/com/dzcode/springboot/masterclass/aspect/CommonJoinPointConfig.java
@@ -8,4 +8,13 @@ public class CommonJoinPointConfig {
 
     @Pointcut("execution(* com.dzcode.springboot.masterclass.business.*.*(..))")
     public void businessLayerExecution() {}
+
+    @Pointcut("com.dzcode.springboot.masterclass.aspect.CommonJoinPointConfig.businessLayerExecution() && com.dzcode.springboot.masterclass.aspect.CommonJoinPointConfig.dataLayerExecution()")
+    public void allLayerExecution() {}
+
+    @Pointcut("bean(*dao*)")
+    public void beanContainingDao() {}
+
+    @Pointcut("within(com.dzcode.springboot.masterclass.data..*)")
+    public void dataLayerExecutionWithWithin() {}
 }

7efeacf8c2919e1fd24bfb88545b3830a9a3ac46
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-18 09:50:49 +0100

    CRUD with spring jdbc

--- src/main/java/com/dzcode/springboot/masterclass/SpringbootMasterclassApplication.java
+++ src/main/java/com/dzcode/springboot/masterclass/SpringbootMasterclassApplication.java
@@ -1,5 +1,6 @@
 package com.dzcode.springboot.masterclass;
 
+import com.dzcode.springboot.masterclass.entity.Person;
 import com.dzcode.springboot.masterclass.jdbc.PersonJdbcDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringbootMasterclassApplication implements CommandLineRunner {
@@ -23,5 +26,9 @@ public class SpringbootMasterclassApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info("All persons -> {}", personJdbcDao.findAll());
+		logger.info("create 10004 -> no of row created - {}", personJdbcDao.create(new Person(10004, "James", "New York", new Date())));
+		logger.info("retrieve person by id 10001 -> {}", personJdbcDao.findById(10001));
+		logger.info("update 10003 -> no of row updated - {}", personJdbcDao.update(new Person(10003, "Ibrahim", "Algiers", new Date())));
+		logger.info("delete 10002 -> no of row deleted - {}", personJdbcDao.deleteById(10002));
 	}
 }
--- src/main/java/com/dzcode/springboot/masterclass/jdbc/PersonJdbcDao.java
+++ src/main/java/com/dzcode/springboot/masterclass/jdbc/PersonJdbcDao.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -15,6 +16,22 @@ public class PersonJdbcDao {
     JdbcTemplate jdbcTemplate;
 
     public List<Person> findAll() {
-        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper(Person.class));
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<Person>(Person.class));
+    }
+
+    public Person findById(int id) {
+        return jdbcTemplate.queryForObject("select * from person where id=?", new Object[]{id}, new BeanPropertyRowMapper<Person>(Person.class));
+    }
+
+    public int deleteById(int id) {
+        return jdbcTemplate.update("delete from person where id=?", new Object[]{id});
+    }
+
+    public int update(Person person) {
+        return jdbcTemplate.update("update person set name=?, location=?, birth_date=? where id=?", new Object[]{person.getName(), person.getLocation(), new Timestamp(person.getBirthData().getTime()), person.getId()});
+    }
+
+    public int create(Person person) {
+        return jdbcTemplate.update("insert into person (id, name, location, birth_date) values (?, ?, ?, ?)", new Object[]{person.getId(), person.getLocation(), person.getLocation(), new Timestamp(person.getBirthData().getTime())});
     }
 }

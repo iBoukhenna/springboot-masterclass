74626100556ff1ef40e1e852bdd2cace2094c293
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-18 10:26:05 +0100

    CRUD with Spring JPA

--- src/main/java/com/dzcode/springboot/masterclass/SpringbootMasterclassJpaApplication.java
+++ src/main/java/com/dzcode/springboot/masterclass/SpringbootMasterclassJpaApplication.java
@@ -1,5 +1,6 @@
 package com.dzcode.springboot.masterclass;
 
+import com.dzcode.springboot.masterclass.entity.User;
 import com.dzcode.springboot.masterclass.jpa.UserJpaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringbootMasterclassJpaApplication implements CommandLineRunner {
@@ -22,6 +25,10 @@ public class SpringbootMasterclassJpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("retrieve person by id 10001 -> {}", userJpaRepository.findById(10001));
+		logger.info("create -> no of row created - {}", userJpaRepository.create(new User("James", "New York", new Date())));
+		logger.info("retrieve person by id 1 -> {}", userJpaRepository.findById(1));
+		logger.info("update 1 -> no of row updated - {}", userJpaRepository.update(new User(1, "Ibrahim", "Algiers", new Date())));
+		logger.info("delete 1 -> no of row deleted");
+		userJpaRepository.deleteById(1);
 	}
 }
--- src/main/java/com/dzcode/springboot/masterclass/jpa/UserJpaRepository.java
+++ src/main/java/com/dzcode/springboot/masterclass/jpa/UserJpaRepository.java
@@ -4,8 +4,10 @@ import com.dzcode.springboot.masterclass.entity.User;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public class UserJpaRepository {
 
     @PersistenceContext
@@ -13,5 +15,18 @@ public class UserJpaRepository {
 
     public User findById(long id) {
         return entityManager.find(User.class, id);
+    }
+
+    public void deleteById(long id) {
+        User user = findById(id);
+        entityManager.remove(user);
+    }
+
+    public User update(User user) {
+        return entityManager.merge(user);
+    }
+
+    public User create(User user) {
+        return entityManager.merge(user);
     }
 }
